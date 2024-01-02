05d6c2917c408ddbd3219bef5a067a653b628c65
josephmaxim <joseph@plasmacreative.com>
2024-01-02 15:53:24 -0500

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 
-import '@/scss/root.scss';
 import 'rsuite/dist/rsuite-no-reset.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/scss/root.scss';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Head>
       <meta charSet="utf-8"/>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=5"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
       <meta name="HandheldFriendly" content="true"/>
 
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
