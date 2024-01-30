27e66ba554fb118e69c12a7cf685e04219762d1a
josephmaxim <joseph@plasmacreative.com>
2024-01-30 16:10:09 -0500

    Add guidelines layout component

---
+++ components/Layouts/GuidelinesLayout.js
@@ -0,0 +1,20 @@
+import MainLayout from "./MainLayout";
+import GuidelinesNav from "../Nav/GuidelinesNav";
+
+const GuidelinesLayout = (props) => {
+
+  return <MainLayout>
+    <div className="guidelines-layout">
+      <div className="container">
+        <div className="content-nav">
+          <GuidelinesNav/>
+        </div>
+        <div className="content-wrapper">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+}
+
+export default GuidelinesLayout;
