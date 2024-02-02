be083706f3280c44c8994d63758584efac0f06c2
josephmaxim <joseph@plasmacreative.com>
2024-02-02 01:50:44 -0500

    .

--- controller/student.js
+++ controller/student.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 const rootPath = '/api/v1';
+import { notify } from "@/utils/notification";
 
 export const submitRegistration = async (form) => {
   try {
     const { data } = await axios.post(`${rootPath}/student/register`, form)
+    notify({
+      message: 'Registration successfully submitted.',
+      type: "success"
+    })
     return data
   } catch (error) {
     notify({
