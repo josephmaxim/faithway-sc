0c95c728cf282b0680e1049d82c878366b2967c8
josephmaxim <joseph@plasmacreative.com>
2024-02-02 20:17:44 -0500

    fix up navigation

--- components/Nav/MainNav.js
+++ components/Nav/MainNav.js
@@ -8,15 +8,52 @@ import {
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem
+  NavItem,
+  Container
 } from 'reactstrap';
 
 const MainNav = () => {
 
-  const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   const NavLink = (props) => <Link {...props} className="nav-link">{props.children}</Link>
+
+  return <div className="main-nav">
+      <Navbar
+        color="faded"
+        // fixed="top"
+        expand="md"
+        container={false}
+        // dark={router.pathname === '/' ? false : true}
+        // className={pageScrolled ? "scrolled" : ""}
+      >
+        <Container>
+          <Link href="/" className="navbar-brand"><MainLogo/></Link>
+          <NavbarToggler onClick={toggleNavbar} className="toggler" />
+          <Collapse isOpen={collapsed} navbar>
+              <hr/>
+              <Nav navbar className="ms-auto">
+                <NavItem>
+                  <NavLink href="/">
+                    Registration
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/billeting">
+                    Billeting Form
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/guidelines">
+                    Student Guidelines
+                  </NavLink>
+                </NavItem>
+              </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
 
   return <div className="main-nav">
     <div className="container">
