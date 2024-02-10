89e4a5feba9cbdb82b76eb22323727e5c159f33a
josephmaxim <joseph@plasmacreative.com>
2024-02-09 22:37:53 -0500

    create script that adds users

---
+++ scripts/initUsers.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+const connectToDatabase = require('#db/index.js')
+const bcrypt = require('bcryptjs')
+const User = require('#db/models/users.js')
+
+async function initUsers(){
+
+  const userList = [
+    {
+      fullName: "Debra Lindhorst",
+      email: 'dlindhorst@faithway.org',
+      password: 'Godi$love@1'
+    },
+    {
+      fullName: "Ray Aquino",
+      email: 'familyaquino@rogers.com',
+      password: 'Godi$love@2'
+    },
+    {
+      fullName: "Joseph Maxim",
+      email: 'joseph@plasmacreative.com',
+      password: 'Godi$love@3'
+    }
+  ];
+
+  await connectToDatabase();
+
+  userList.map(async (user) => {
+    
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(user.password, salt);
+
+    const isExist = await User.findOne({email: user.email});
+
+    if(isExist) return user;
+
+    const newUser = new User({
+      ...user,
+      password,
+      permissions: ['admin']
+    })
+
+    await newUser.save();
+  })
+}
+
+initUsers();
