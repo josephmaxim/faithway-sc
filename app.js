67459428c1a639430716889743dd7ac76a64b738
josephmaxim <joseph@plasmacreative.com>
2024-02-09 22:35:28 -0500

    Added passport functions and page redirections

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const next = require('next')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const passport = require('passport')
 
 const { isDev, urlHost } = require('#utils/commons.js')
 const env = process.env
 const app = next({ dev: isDev })
 const handle = app.getRequestHandler()
 const connectToDatabase = require('./db/index')
+const User = require('#db/models/users.js')
+const localAuthStrategy = require('#server/lib/localAuthStrategy.js')
+const securedPages = require('#utils/securedPages.js')
 
 const routes = require('./server/routes')
 
@@ -21,6 +28,22 @@ app.prepare()
   server.use(express.urlencoded({ extended: false }));
   server.use(cookieParser())
 
+  server.use(session({
+    secret: env.SESSION_SECRET,
+    resave: true,
+    proxy: true,
+    saveUninitialized: false,
+    cookie:{
+      maxAge: 1000 * 60 * 60 * 24 * 15,
+      // sameSite: true,
+      secure: isDev ? false : true,
+      httpOnly: true,
+    },
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    })
+  }));
+
   server.use((req, res, next) => {
     res.set('credentials', 'include');
     res.set('Access-Control-Allow-Credentials', true);
@@ -30,9 +53,20 @@ app.prepare()
     return next();
   });
 
+  server.use(passport.initialize());
+  server.use(passport.session());
+  passport.use(localAuthStrategy);
+  passport.serializeUser((user, done) => {done(null, user._id)})
+  passport.deserializeUser(async (id, done) => {done (null, await User.findById(id).select('-__v'))}) 
+
   server.use(routes)
 
   server.get('*', (req, res) => {
+    const {path} = req
+    const redirectPath = urlHost + req.url;
+
+    if(securedPages.includes(path) && !req.isAuthenticated()) return res.redirect(302, `/login?redirect=${encodeURIComponent(redirectPath)}`)
+    if(path == '/login' && req.isAuthenticated()) return res.redirect(302, '/dashboard')
     return handle(req, res)
   })
 
