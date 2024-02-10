7f0e70c05466f56859ea1f718f54709ab5fa7959
josephmaxim <joseph@plasmacreative.com>
2024-02-09 22:33:28 -0500

    Added auth check endpoint

--- server/routes/api/v1/auth/index.js
+++ server/routes/api/v1/auth/index.js
@@ -1,8 +1,16 @@
 const route = require('express').Router()
+const securedRoute = require('#server/middleware/securedRoute.js')
 
 // returns user data
-// route.get('/', (req, res, next) => {
-//   return res.json('cool')
-// });
+route.get('/', securedRoute, (req, res, next) => {
+  const userData = {...req.user._doc}
+
+  delete userData.password
+
+  return res.json(userData)
+});
+
+route.use('/login', require('./login'));
+route.use('/logout', require('./logout'));
 
 module.exports = route;
