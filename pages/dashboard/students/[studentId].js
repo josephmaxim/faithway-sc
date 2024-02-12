6585feb09490f62cdd79dce8c1fdfde4992fa450
josephmaxim <joseph@plasmacreative.com>
2024-02-12 11:19:50 -0500

    .

---
+++ pages/dashboard/students/[studentId].js
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/components/Layouts/DashboardLayout";
+
+const Student = () => {
+  return <DashboardLayout>
+    <div className="container">
+      <h1>Student Page</h1>
+      <p>Still working on it :)</p>
+    </div>
+  </DashboardLayout>
+}
+
+export default Student;
