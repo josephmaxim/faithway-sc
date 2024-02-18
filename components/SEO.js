f21ba0664e466e1472836473109c3aafd91ae2fc
josephmaxim <joseph@plasmacreative.com>
2024-02-19 06:33:56 +0800

    Added SEO component

---
+++ components/SEO.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+const HeadTags = (props) => {
+
+  const metaImage = props.metaImage || '/img/default_meta.png';
+
+  return <Head>
+    <title>{props.title}</title>
+    <meta name="description" content={props.description} />
+    <meta property="og:site_name" content="Christian Student Convention" />
+    <meta property="og:url" content={props.url} />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content={props.title} />
+    <meta property="og:description" content={props.description} />
+    <meta property="og:image" content={metaImage}  />
+    <meta property="og:image:secure_url" content={metaImage} />
+    <meta property="og:image:width" content="600" />
+    <meta property="og:image:height" content="500" />
+    <link rel="canonical" href={props.url} />
+    {props.children}
+  </Head>
+}
+
+export default HeadTags;
