ec972f553451fcf16477a335a95287c3fbe98a5d
josephmaxim <joseph@plasmacreative.com>
2024-02-20 16:10:36 +0800

    Added SEO component to main layout

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -2,10 +2,14 @@ import Link from 'next/link';
 
 import MainNav from '../Nav/MainNav';
 import MainFooter from '@/components/Footers/MainFooter';
+import HeadTags from '@/components/SEO';
 
 const MainLayout = (props) => {
 
   return <main className="main-layout">
+    <HeadTags
+      {...props.head}
+    />
     <MainNav/>
     {
       props.header ? 
